d5719b818955204bcf2498b3bc3afb2c36bf847b
Serhat Can <serhat.can@securitas.com>
2026-06-04 14:48:07 +0200

    fix naming

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ class WebView extends ItemView {
         });
 
         this.wv.addEventListener('new-window', (e) => {
-            const plugin = this.app.plugins.getPlugin('web-view');
+            const plugin = this.app.plugins.getPlugin('obsidian-web-view');
             if (plugin) plugin.openUrl(e.url);
         });
     }
